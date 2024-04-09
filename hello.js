4403b0be74b698fefcc33b94aecbc1229ceb1eb0
Farhin Rahman <farhin.rahmanp@gmail.com>
2024-04-10 01:25:19 +0600

    fourth commit

---
+++ hello.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    var toggleBtn = document.getElementById('toggle');
+    var collapseMenu = document.getElementById('collapseMenu');
+    
+    function handleClick() {
+      if (collapseMenu.style.display === 'block') {
+        collapseMenu.style.display = 'none';
+      } else {
+        collapseMenu.style.display = 'block';
+      }
+    }
+    
+    toggleBtn.addEventListener('click', handleClick);
+  });
